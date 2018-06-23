fdfff84d11da7a62d8c14851a67822a3699576f0
Jari Mäkelä <jari.makela@reaktor.com>
2018-06-23 14:01:06 +0300

    Debug events

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ app.use(express.json())
 app.get("/", (req, res) => res.send("Hello World!"))
 
 app.post("/event", (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(req.body))
   if (req.body.type === "url_verification") {
     res.setHeader("Content-Type", "text/plain")
     res.send(req.body.challenge)
