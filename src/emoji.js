c6dca36661e625d2e30bf8e57e54f79b4777a0c0
Jari Mäkelä <jari.makela@reaktor.com>
2018-06-23 23:30:34 +0300

    Add emoji lib

---
+++ src/emoji.js
@@ -0,0 +1,39 @@
+import Jimp from "jimp"
+
+const emojis = {
+  allislost: true,
+  cry: true,
+  emoji: true,
+  finger: true,
+  hmm: true,
+  hug: true,
+  intense: true,
+  joy: true,
+  karhu: true,
+  laugh: true,
+  ping: true,
+  sheep: true,
+  toast: true
+}
+
+export function getEmoji(s) {
+  const match = s.match(/:[a-z\-0-9]+:/)
+  if (match) {
+    return match[0].replace(/:/g, "").split("-")
+  }
+  return null
+}
+
+export function unknowns(emoji) {
+  return emoji.filter(e => !emojis[e])
+}
+
+export async function create(emoji) {
+  const images = await Promise.all(emoji.map(e => Jimp.read(`images/${e}.png`)))
+  return images.reduce((image, e) => {
+    if (image) {
+      return image.composite(e, 0, 0)
+    }
+    return e
+  })
+}
---
+++ test/emoji.test.js
@@ -0,0 +1,32 @@
+import "jest"
+import { getEmoji, unknowns } from "../src/emoji"
+
+describe("getEmoji", () => {
+  it("finds emoji", () => {
+    expect(getEmoji(":joo:")).toEqual(["joo"])
+    expect(getEmoji("asd :joo: asdsa")).toEqual(["joo"])
+  })
+
+  it("parses emoji parts", () => {
+    expect(getEmoji(":joo-bar-foo:")).toEqual(["joo", "bar", "foo"])
+    expect(getEmoji("asd :joo-asdf2-goo: asdsa")).toEqual([
+      "joo",
+      "asdf2",
+      "goo"
+    ])
+  })
+
+  it("returns null if nothing is found", () => {
+    expect(getEmoji(":joo")).toEqual(null)
+    expect(getEmoji("asd joo: asdsa")).toEqual(null)
+  })
+})
+
+describe("unknowns", () => {
+  it("finds unknown elements", () => {
+    expect(unknowns(["hmm", "karhu", "foo", "bar", "allislost"])).toEqual([
+      "foo",
+      "bar"
+    ])
+  })
+})
